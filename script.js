a01272b78024c790c2bc5fc23a198dfb79ca7f31
FrichHapita <friedrichhapita82@gmail.com>
2022-06-27 09:33:22 +0800

    First Deploy

---
+++ script.js
@@ -0,0 +1,7 @@
+const script = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if(link.href.includes(`${script}`)){
+    link.classList.add('active');
+    console.log(link);
+  }
+})
